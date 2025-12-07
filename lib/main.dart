d4f7ec39634338890393fccb06a2b49f17e0d6e3
Roberto Alejandro Romero Torres <robertortups@gmail.com>
2025-12-07 00:05:56 -0500

    feat: inicializar firebase core y habilitar modo desarrollador
    - Se agregó la dependencia firebase_core.
    - Se actualizó main.dart para inicializar Firebase de forma asíncrona.
    - Se verificó la conexión exitosa mediante un mensaje en consola.

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  // --- LÍNEAS NUEVAS DE VERIFICACIÓN ---
+  print("-------------------------------------------");
+  print("🚀 ¡ÉXITO! FIREBASE SE INICIÓ CORRECTAMENTE 🚀");
+  print("-------------------------------------------");
+  // ----------------------------------------
+
   runApp(const MyApp());
 }
 
